8509ee04b3ff7a54abc3e751e8ca0c6a1a27d29b
BaeWaterLeo <hoanglonggg084@gmail.com>
2021-10-15 14:04:38 +0700

    15 Oct 1404

--- models/dk_add_district.py
+++ models/dk_add_district.py
@@ -1,33 +1,4 @@
-from odoo import api, fields, models<?xml version="1.0"?>
-<odoo>
-    <data>
-
-        <record id="view_sale_order_add_city" model="ir.ui.view">
-            <field name="name">sale.order.add.city.form</field>
-            <field name="model">sale.order</field>
-            <field name="inherit_id" ref="sale.view_order_form"/>
-            <field name="arch" type="xml">
-                <xpath expr="//field[@name='partner_id']" position="after">
-
-                                    <field name="country_id" string="Country"/>
-                                    <field name="city_id" string="City"
-                                           domain="[('country_id', '=', country_id)]"/>
-                                    <field name="district_id" string="District"
-                                           domain="[('city_id', '=', city_id)]"/>
-                                    <field name="name" string="Ward"
-                                           domain="[('district_id', '=', district_id)]"/>
-
-                <field name="loai_don_hang" string="Ward"/>
-                <field name="name" string="Ward"/>
-                <field name="country_id" string="Country"/>
-                <field name="city_id" string="City"/>
-                <field name="district_id" string="District"/>
-                <field name="name" string="Ward"/>
-            </field>
-        </record>
-
-    </data>
-</odoo>
+from odoo import api, fields, models
 
 
 class AddDistrict(models.Model):
