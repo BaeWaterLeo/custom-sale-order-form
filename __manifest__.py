3f92fe3969ffd935165ba59436ed56178a64cb6b
BaeWaterLeo <hoanglonggg084@gmail.com>
2021-10-18 15:25:22 +0700

    18 Oct 1525 - completed

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,8 @@
     "depends": ["base", "sale"],
     "data": [
         "views/customize_sale_order_view.xml",
-        "views/create_location.xml"
+        "views/create_location.xml",
+        "report/customize_sale_order_report_view.xml"
     ],
     "license": "AGPL-3",
     "installable": True,
